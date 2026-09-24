424ec2b897b010a9997b34d9a42b08be51d61a9f
Isabela Clímaco <isabelasclimaco@gmail.com>
2026-09-24 19:32:07 -0300

    Cria aplicação RottenPotatoes inicial
    - Projeto Rails 8.1.3.1 com SQLite e haml-rails
    - Scaffold de Movie (title, rating, description, release_date) com views .html.haml
    - resources :movies em routes.rb e migração da tabela movies
    - Validações no model: title presence, rating inclusion em RATINGS, release_date presence
    - Constante RATINGS = %w[G PG PG-13 R NC-17]
    - Layout global em Haml com flash messages e #main
    - CSS completo (body, tabela #movies, formulários, mensagens flash)
    - Select de rating no formulário usando Movie::RATINGS
    - Seeds com 3 filmes (find_or_create_by! para idempotência)
    - Testes do model: título obrigatório e rating inválido rejeitado
    - .gitignore cobre SQLite, master.key e .env

---
+++ test/models/movie_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class MovieTest < ActiveSupport::TestCase
+  test "precisa ter título" do
+    movie = Movie.new(rating: "PG", release_date: Date.current)
+    assert_not movie.valid?
+    assert_includes movie.errors[:title], "can't be blank"
+  end
+
+  test "aceita classificações conhecidas" do
+    movie = Movie.new(title: "Filme", rating: "INVALIDA",
+                      release_date: Date.current)
+    assert_not movie.valid?
+  end
+end
