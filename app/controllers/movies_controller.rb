25ef18540c06312edfe58a9b185a44783ae6f4d6
Isabela Clímaco <isabelasclimaco@gmail.com>
2026-09-24 19:36:34 -0300

    Adiciona ordenação segura e destaque de coluna
    - Controller: captura params[:sort_by] e valida contra allowed_sorts
      para prevenir injeção de colunas arbitrárias via URL
    - Index: tabela com links link_to no título e na data de lançamento
      (ambos apontam para a página show do filme)
    - Headers "Título" e "Lançamento" são links com id :title_header e
      :release_date_header, passando sort_by como parâmetro na rota
    - Coluna ativa recebe class="hilite" → fundo dourado via CSS
    - Fixtures corrigidos com ratings válidos (G e R) para passar validações

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,9 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    allowed_sorts = %w[title release_date]
+    @sort_by = params[:sort_by] if allowed_sorts.include?(params[:sort_by])
+    @movies = @sort_by ? Movie.order(@sort_by) : Movie.all
   end
 
   # GET /movies/1 or /movies/1.json
